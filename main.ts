18c94195f8fcd29e4ef3ddc90eec2035225bb02e
SeardnaSchmid <as@as.as>
2024-08-25 11:38:51 +0200

    working recursive content copy

--- main.ts
+++ main.ts
@@ -1,134 +1,73 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { Notice, Plugin, TAbstractFile, TFile, TFolder } from "obsidian";
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
+interface FileInfo {
+	path: string;
+	name: string;
+	content: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+interface FolderInfo {
+	name: string;
+	files: FileInfo[];
+	folders: FolderInfo[];
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
-
+export default class RecursiveFolderCopyPlugin extends Plugin {
 	async onload() {
-		await this.loadSettings();
+		this.registerEvent(
+			this.app.workspace.on("file-menu", (menu, file) => {
+				menu.addItem((item) => {
+					item
+						.setTitle("Copy Folder Contents")
+						.setIcon("copy")
+						.onClick(async () => {
+							await this.copyContents(file);
+						});
+				});
+			})
+		);
+	}
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+	async copyContents(file: TAbstractFile) {
+		if (file instanceof TFolder) {
+			const contents = await this.getFolderContents(file);
+			await navigator.clipboard.writeText(JSON.stringify({ root: file.name, ...contents }, null, 2));
+			new Notice("Folder contents copied to clipboard in JSON format!");
+		} else if (file instanceof TFile) {
+			const content = await this.app.vault.read(file);
+			const fileInfo: FileInfo = {
+				path: file.path,
+				name: file.name,
+				content: content
+			};
+			await navigator.clipboard.writeText(JSON.stringify(fileInfo, null, 2));
+			new Notice("File content copied to clipboard in JSON format!");
+		} else {
+			new Notice("Unable to copy. Selected item is neither a file nor a folder.");
+		}
+	}
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+	async getFolderContents(folder: TFolder): Promise<FolderInfo> {
+		const folderInfo: FolderInfo = {
+			name: folder.name,
+			files: [],
+			folders: []
+		};
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
+		for (const child of folder.children) {
+			if (child instanceof TFile) {
+				const content = await this.app.vault.read(child);
+				folderInfo.files.push({
+					path: child.path,
+					name: child.name,
+					content: content
+				});
+			} else if (child instanceof TFolder) {
+				const subFolder = await this.getFolderContents(child);
+				folderInfo.folders.push(subFolder);
 			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
+		}
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
-
-	onunload() {
-
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+		return folderInfo;
 	}
 }
