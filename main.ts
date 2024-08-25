b99f39db7e9f099d804b36c3651b72f2b4c6f245
SeardnaSchmid <as@as.as>
2024-08-25 11:38:51 +0200

    final version with comments

--- main.ts
+++ main.ts
@@ -1,73 +1,112 @@
 import { Notice, Plugin, TAbstractFile, TFile, TFolder } from "obsidian";
 
-interface FileInfo {
-	path: string;
-	name: string;
-	content: string;
+// Enum to distinguish between file and folder types
+enum ItemType {
+    File = 'file',
+    Folder = 'folder'
 }
 
-interface FolderInfo {
-	name: string;
-	files: FileInfo[];
-	folders: FolderInfo[];
+// Base interface for both file and folder items
+interface BaseItem {
+    type: ItemType;
+    path: string;
 }
 
-export default class RecursiveFolderCopyPlugin extends Plugin {
-	async onload() {
-		this.registerEvent(
-			this.app.workspace.on("file-menu", (menu, file) => {
-				menu.addItem((item) => {
-					item
-						.setTitle("Copy Folder Contents")
-						.setIcon("copy")
-						.onClick(async () => {
-							await this.copyContents(file);
-						});
-				});
-			})
-		);
-	}
+// Interface for file items, extending BaseItem
+interface FileItem extends BaseItem {
+    type: ItemType.File;
+    content: string;
+}
 
-	async copyContents(file: TAbstractFile) {
-		if (file instanceof TFolder) {
-			const contents = await this.getFolderContents(file);
-			await navigator.clipboard.writeText(JSON.stringify({ root: file.name, ...contents }, null, 2));
-			new Notice("Folder contents copied to clipboard in JSON format!");
-		} else if (file instanceof TFile) {
-			const content = await this.app.vault.read(file);
-			const fileInfo: FileInfo = {
-				path: file.path,
-				name: file.name,
-				content: content
-			};
-			await navigator.clipboard.writeText(JSON.stringify(fileInfo, null, 2));
-			new Notice("File content copied to clipboard in JSON format!");
-		} else {
-			new Notice("Unable to copy. Selected item is neither a file nor a folder.");
-		}
-	}
+// Interface for folder items, extending BaseItem
+interface FolderItem extends BaseItem {
+    type: ItemType.Folder;
+    children: FileSystemItem[];
+}
 
-	async getFolderContents(folder: TFolder): Promise<FolderInfo> {
-		const folderInfo: FolderInfo = {
-			name: folder.name,
-			files: [],
-			folders: []
-		};
+// Union type for either a file or folder item
+type FileSystemItem = FileItem | FolderItem;
 
-		for (const child of folder.children) {
-			if (child instanceof TFile) {
-				const content = await this.app.vault.read(child);
-				folderInfo.files.push({
-					path: child.path,
-					name: child.name,
-					content: content
-				});
-			} else if (child instanceof TFolder) {
-				const subFolder = await this.getFolderContents(child);
-				folderInfo.folders.push(subFolder);
-			}
-		}
+// Interface for the final copy information structure
+interface CopyInfo {
+    rootElement: FileSystemItem;
+    timestamp: string;
+}
 
-		return folderInfo;
-	}
+export default class StreamlinedFolderCopyPlugin extends Plugin {
+    async onload() {
+        // Register a new item in the file menu
+        this.registerEvent(
+            this.app.workspace.on("file-menu", (menu, file) => {
+                menu.addItem((item) => {
+                    item
+                        .setTitle("Copy Contents")
+                        .setIcon("copy")
+                        .onClick(async () => {
+                            await this.copyContents(file);
+                        });
+                });
+            })
+        );
+    }
+
+    // Main method to copy contents of a file or folder
+    async copyContents(file: TAbstractFile) {
+        let rootFolder: FileSystemItem;
+
+        if (file instanceof TFolder) {
+            // If it's a folder, get its contents recursively
+            rootFolder = await this.getFolderContents(file);
+        } else if (file instanceof TFile) {
+            // If it's a file, read its content
+            const content = await this.app.vault.read(file);
+            rootFolder = {
+                type: ItemType.File,
+                path: file.path,
+                content: content
+            };
+        } else {
+            // If it's neither a file nor a folder, show an error
+            new Notice("Unable to copy. Selected item is neither a file nor a folder.");
+            return;
+        }
+
+        // Create the final copy info structure
+        const copyInfo: CopyInfo = {
+            rootElement: rootFolder,
+            timestamp: new Date().toISOString()
+        };
+
+        // Copy the stringified JSON to clipboard
+        await navigator.clipboard.writeText(JSON.stringify(copyInfo, null, 2));
+        new Notice("Contents copied to clipboard in JSON format!");
+    }
+
+    // Recursive method to get contents of a folder
+    async getFolderContents(folder: TFolder): Promise<FileSystemItem> {
+        const folderItem: FileSystemItem = {
+            type: ItemType.Folder,
+            path: folder.path,
+            children: []
+        };
+
+        // Iterate through all children of the folder
+        for (const child of folder.children) {
+            if (child instanceof TFile) {
+                // If child is a file, read its content
+                const content = await this.app.vault.read(child);
+                folderItem.children!.push({
+                    type: ItemType.File,
+                    path: child.path,
+                    content: content
+                });
+            } else if (child instanceof TFolder) {
+                // If child is a folder, recursively get its contents
+                const subFolderItem = await this.getFolderContents(child);
+                folderItem.children!.push(subFolderItem);
+            }
+        }
+
+        return folderItem;
+    }
 }
